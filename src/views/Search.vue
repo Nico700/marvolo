7c180fa18475204b1b0c7e4122592b13e9d415dc
Pierre <pierre.iaccarino@ynov.com>
2020-10-15 11:21:19 +0200

    Début API

---
+++ src/views/Search.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+      <h1>Resultats</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Home",
+  methods: {
+    getResult() {
+      console.log('les resultats');
+    }
+  }
+};
+</script>
