2c26f4229567c93849c3b29b2901d7a1c8e25411
Pierre <pierre.iaccarino@ynov.com>
2020-10-15 15:21:16 +0200

    Infos API

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import Search from "../views/Search.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: "/search",
+    name: "Search",
+    component: Search,
+    props: true
   }
 ];
 
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,17 +1,44 @@
 <template>
   <div class="home">
-      <h1>Resultats</h1>
+    <h1>Resultats</h1>
   </div>
 </template>
 
 <script>
+const axios = require("axios");
+const md5 = require('md5');
 
 export default {
-  name: "Home",
+  name: "Search",
+  data() {
+    return {
+      search: this.$props,
+      url: "https://gateway.marvel.com:443/v1/public",
+      apikey: process.env.VUE_APP_APIKEY_PUBLIC,
+      apikeyPrivate: process.env.VUE_APP_APIKEY_PRIVATE,
+      hash: md5(
+        "1" +
+          process.env.VUE_APP_APIKEY_PRIVATE +
+          process.env.VUE_APP_APIKEY_PUBLIC
+      ),
+      search_content: this.$refs.valeur_search
+    };
+  },
   methods: {
-    getResult() {
-      console.log('les resultats');
+    getHeroes: function() {
+      console.log(this.search);
+      axios
+        .get(this.url + "/characters?nameStartsWith=sp&ts=1&apikey=" + this.apikey + "&hash=" + this.hash)
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
     }
+  },
+  beforeMount() {
+    this.getHeroes();
   }
 };
 </script>
